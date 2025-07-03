1c1d381d59eb04db290f1326d85a8c701a0a4ecb
DINESH480 <kummaridinesh37@gmail.com>
2025-07-03 21:50:20 +0530

    adding numbers

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,8 +1,9 @@
-import random
-import string
-
-def generate_password(length):
-    chars = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(random.choice(chars) for _ in range(length))
-
-print(generate_password(12))
+a=2
+b=4
+c=3
+a=a+b
+b=b+c
+c=c+a
+print(a)
+print(b)
+print(c)
