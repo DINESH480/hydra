ba7d33974ccc86c8b8234c57625a088144c18c30
DINESH480 <kummaridinesh37@gmail.com>
2025-07-20 21:44:05 +0530

    even or odd1

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,14 +1,12 @@
-import random
-choices = ['rock', 'paper', 'scissors']
-user = input("Choose rock/paper/scissors: ").lower()
-bot = random.choice(choices)
-print(f"You: {user} | Bot: {bot}")
-if user == bot:
-    print("Draw")
-elif (user == 'rock' and bot == 'scissors') or \
-     (user == 'paper' and bot == 'rock') or \
-     (user == 'scissors' and bot == 'paper'):
-    print("You win!")
+n=int(input("enter a nuber"))
+if n%2 == 0:
+    print("even")
 else:
-    print("You lose!")
-print("Thanks for playing!")
+    print("odd")
+    if n>0:
+        print("positive")
+    else:
+        print("negative")
+
+if n == 0:
+    print("zero") 
