02c542cdad955348661e2a74f7fa9ff8ae2112ec
DINESH480 <kummaridinesh37@gmail.com>
2025-07-24 23:20:59 +0530

    strength of password

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,12 +1,17 @@
-n=int(input("enter a nuber"))
-if n%2 == 0:
-    print("even")
-else:
-    print("odd")
-    if n>0:
-        print("positive")
-    else:
-        print("negative")
+import re
 
-if n == 0:
-    print("zero") 
+def check_strength(password):
+    if len(password) < 8:
+        return "Weak: Too short"
+    if not re.search("[a-z]", password):
+        return "Weak: Missing lowercase"
+    if not re.search("[A-Z]", password):
+        return "Weak: Missing uppercase"
+    if not re.search("[0-9]", password):
+        return "Weak: Missing digit"
+    if not re.search("[!@#$%^&*()_+]", password):
+        return "Weak: Missing special character"
+    return "Strong Password!"
+
+password = input("Enter your password: ")
+print(check_strength(password))
